17baafd2b66ca6148d9661a562a5921e352d787e
saidyl <ghasemialisa1@gmail.com>
2026-09-18 11:24:37 +0330

    Update PeopleFragment.kt

--- app/src/main/java/yadetbashe/alisa/ui/fragments/PeopleFragment.kt
+++ app/src/main/java/yadetbashe/alisa/ui/fragments/PeopleFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavGraphDirections
+import yadetbashe.app.alisa.NavGraphDirections
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
